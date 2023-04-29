003f188299ac711bff9bdc3bfe59b48e76664548
abubakarMana01 <ym.abubakr7@gmail.com>
2023-04-29 12:38:36 +0100

    fix: minor...

--- src/hooks/useAuthToken.tsx
+++ src/hooks/useAuthToken.tsx
@@ -21,5 +21,5 @@ export default function useAuthToken() {
   const removeToken = () => {
     AsyncStorage.removeItem('jwtToken');
   };
-  return {storeToken, removeToken, getToken};
+  return {storeToken, getToken, removeToken};
 }
